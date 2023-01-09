a712a28b8e3fed0efd12ec30e547fb97cf49e41f
Mateus Romão <m.romao.ds@gmail.com>
2023-01-09 16:48:58 -0300

    feat: create player

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useCallback } from 'react';
+import Particles from "react-particles";
+import { loadFull } from 'tsparticles';
+import type { Engine } from "tsparticles-engine";
 import './App.css';
+import { Player } from './components/Player';
 
 function App() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+}, []);
+
   return (
     <div className="App">
+      <Particles
+        className='particles'
+        init={particlesInit}
+        options={{
+          background: {
+              color: {
+                  value: "#2A2141",
+              },
+          },
+          fpsLimit: 120,
+          interactivity: {
+              events: {
+                  onClick: {
+                      enable: true,
+                      mode: "repulse",
+                  },
+                  onHover: {
+                      enable: false,
+                  },
+                  resize: true,
+              },
+              modes: {
+                  push: {
+                      quantity: 4,
+                  },
+                  repulse: {
+                      distance: 200,
+                      duration: 0.4,
+                  },
+              },
+          },
+          particles: {
+              color: {
+                  value: "#ffffff",
+              },
+              collisions: {
+                  enable: true,
+              },
+              move: {
+                  direction: "none",
+                  enable: true,
+                  outModes: {
+                      default: "bounce",
+                  },
+                  random: true,
+                  speed: 2,
+                  straight: false,
+              },
+              number: {
+                  density: {
+                      enable: true,
+                      area: 800,
+                  },
+                  value: 100,
+              },
+              opacity: {
+                  value: 0.5,
+              },
+              shape: {
+                  type: "circle",
+              },
+              size: {
+                  value: { min: 1, max: 5 },
+              },
+          },
+          detectRetina: true,
+      }}
+      />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Player />
       </header>
     </div>
   );
---
+++ src/components/Player/index.tsx
@@ -0,0 +1,77 @@
+
+import { useEffect, useRef, useState } from 'react';
+import { BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
+
+import Slider from 'rc-slider';
+
+import 'rc-slider/assets/index.css';
+import './styles.css';
+
+export function Player() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(50);
+
+  useEffect(() => {
+    if(!audioRef.current){
+      return
+    }
+    if(isPlaying){
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+
+  }, [isPlaying]);
+
+
+  function handleSeek(amount: number | number[]) {
+    if (audioRef.current && typeof amount === 'number') {
+      audioRef.current.volume = amount / 100;
+      setVolume(amount);
+    }
+  }
+
+  return (
+    <div className='container'>
+        <div className='image-container'>
+          <img src="/assets/splash.svg" alt="splash"  unselectable="on" />
+        </div>
+        <div className="title-container">
+          <div title="title=artist" unselectable="on">
+            Lofi Hip Hop Mix
+          </div>
+          <div className="title-name" unselectable="on">
+            Dia de chuva
+          </div>
+        </div>
+        <div>
+          <Slider
+            max={100}
+            value={volume}
+            onChange={handleSeek}
+            trackStyle={{backgroundColor: '#6E49BF' }}
+            railStyle={{backgroundColor: '#B9A3D9'}}
+            handleStyle={{borderColor: '#6E49BF', borderWidth: 4}}
+          />
+        </div>
+        <audio 
+          src="assets/music.mp3"
+          ref={audioRef}
+          loop
+          onPlay={() => setIsPlaying(true)}
+          onPause={() => setIsPlaying(false)}
+        />
+        <button
+          className='button-player'
+          onClick={() => setIsPlaying((prevState) => !prevState)}
+        >
+          {isPlaying ? (
+            <BsFillPauseFill size={30} color="#ffffff" />
+          ):(
+            <BsFillPlayFill size={30} color="#ffffff" />
+          )}
+        </button>
+    </div>
+  )
+}
